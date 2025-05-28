4dfdd9102d888920bc5466778f2bbc98965b8500
zinhle <ngwanejeanette3@gmail.com>
2025-05-28 02:24:02 +0200

    created an array and passed props

--- src/components/bagItems.jsx
+++ src/components/bagItems.jsx
@@ -6,10 +6,18 @@ const items =
   [ {
     image: iphone16pro,
     phone: "Apple iphone 16 pro",
-    color: "black",
+    color: "pearl white",
     description: "128GB great camera",
     stars: "5stars",  //will check image for actual stars
     price: "R50000"
+  },
+   {
+    image: iphone11,
+    phone: "Apple iphone 11",
+    color: "all colors",
+    description: "256GB great camera",
+    stars: "4stars",  //will check image for actual stars
+    price: "R39000"
   },
 ]
 return(
@@ -17,22 +25,19 @@ return(
  <div className="text-4xl margin-6">
  <p>Check your Bag Items</p>
  </div>
-<div className="flex items-start gap-6 border-b border-gray-300 py-6"{">"}
-  <img src={iphone16pro} className="w-60"></img>
+ {items.map((items, index) => (
+<div key={index} className="flex items-start gap-6 border-b border-gray-300 py-6 rounded-shadow">
+  <img src={items.image} className="w-60"></img>
   <div className="space-y-2">
-            <h3 className="text-xl font-semibold">{item.phone}</h3>
-            <p className="text-gray-600">{item.color}</p>
-            <p className="text-gray-700">{item.description}</p>
-            <p className="text-yellow-500">{item.stars}</p>
-            <p className="text-green-600 font-bold">{item.price}</p>
+            <h3 className="text-xl font-semibold">{items.phone}</h3>
+            <p className="text-gray-600">{items.color}</p>
+            <p className="text-gray-700">{items.description}</p>
+            <p className="text-yellow-500">{items.stars}</p>
+            <p className="text-green-600 font-bold">{items.price}</p>
           </div>
-         
- 
-
-
-
-
- 
+          
+          </div>
+ ))}
  </div>
 );
 }
