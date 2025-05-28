8447dc226d4fd4fc97772c799b5e46d281e03e03
zinhle <ngwanejeanette3@gmail.com>
2025-05-28 02:10:12 +0200

    created an array

--- src/components/bagItems.jsx
+++ src/components/bagItems.jsx
@@ -2,50 +2,39 @@ import React from "react";
 import { iphone16pro, iphone11, macbook, watchseries7 } from "../images";
 
 function BagItems () {
+const items = 
+  [ {
+    image: iphone16pro,
+    phone: "Apple iphone 16 pro",
+    color: "black",
+    description: "128GB great camera",
+    stars: "5stars",  //will check image for actual stars
+    price: "R50000"
+  },
+]
 return(
- <>
- <div className="bg-red-500 text-white text-4xl p-4">
+<div className="container mx-auto px-4 py-8 " >
+ <div className="text-4xl margin-6">
  <p>Check your Bag Items</p>
  </div>
- <div>
-  <img src={iphone16pro}></img>
-  </div>
-  <div>
-<p>Apple iphone 16pro</p>
-</div>
-<div>
-<p>color</p>
-</div>
-<div>
-<p>short description</p>
-</div>
-<div>
-<p>stars</p>
-</div>
-<div>
-<p>price</p>
- </div>
- <div>
-  <img src={iphone11}></img>
-  </div>
-  <div>
-<p>Apple iphone 11</p>
-</div>
-<div>
-<p>color</p>
-</div>
-<div>
-<p>short description</p>
-</div>
-<div>
-<p>stars</p>
-</div>
-<div>
-<p>price</p>
- </div>
- </>
+<div className="flex items-start gap-6 border-b border-gray-300 py-6"{">"}
+  <img src={iphone16pro} className="w-60"></img>
+  <div className="space-y-2">
+            <h3 className="text-xl font-semibold">{item.phone}</h3>
+            <p className="text-gray-600">{item.color}</p>
+            <p className="text-gray-700">{item.description}</p>
+            <p className="text-yellow-500">{item.stars}</p>
+            <p className="text-green-600 font-bold">{item.price}</p>
+          </div>
+         
  
-)
+
+
+
+
+ 
+ </div>
+);
 }
 
 
